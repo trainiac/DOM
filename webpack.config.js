d64cd56efeb8294cf464b6c38f7844d6d5277801
Adrian Adkison <adrian@Adrians-MacBook-Pro.local>
2017-02-20 21:42:43 -0800

    use es2015 loose

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+/*
+module.exports = {
+  entry: {
+    handleIf: './src/helpers/handleIf.js'
+  },
+  output: {
+    path: path.join(__dirname, './lib'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: path.join(__dirname, './src'),
+      }
+    ]
+  }
+}
+*/
